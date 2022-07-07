9ad5305daf8c5b6eeadcb97d23ef517c7a8c19c8
Tomás Claudeth Sanimbo <tomassanimbo@gmail.com>
2022-07-07 17:03:37 +0100

    Nova page company.js

--- pages/empresas/getmoney.js
+++ pages/empresas/getmoney.js
@@ -1,45 +1,56 @@
-
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 import Script from "next/script";
 
-export default function GetMoney(){
+export default function GetMoney() {
   return (
     <>
-
-    <Script src="/scripts/main.js" strategy="lazyOnload" ></Script>
+      {/* <Script src="/scripts/main.js" strategy="lazyOnload" ></Script> */}
+      <Script src="/scripts/company.js"></Script>
       <div id="Money">
-        <section className="tirarFundo padding" >
+        <section className="tirarFundo padding">
           <div className="container">
-            <h2 id='th' style={{color: "blue"}}>Caelum cursos</h2>
+            {/* <h2 id='th' style={{color: "blue"}}>Caelum cursos</h2> */}
 
-              <div className="container-800">
-                  <div className="texto">
-                    <h1 className="title" >SOLICITAR UM LEVANTAMENTO   </h1>
-                    <p className="description">
-                        O valor solicitado sera transferido para a plataforma principal, <span>EMAINVEST.AO</span>
-                        certifique-se CONSULTAR a transferencia.
-                    </p>
-                  </div>
-
-                <div className="boxMoney">
-                  <h3>Informe a quantidade por favor!</h3>
-                  <form action="POST" className="form">
-                      <div className="form-control">
-                        <input type="text" value={'Ao'}/>
-                        <input required type="number" name="getMoney" id="getMoney" placeholder="2.000.000,00"/>
-                      </div>
-                      <button className="btn-busca" >LEVANTAR</button>
-                  </form>
-                </div>
+            <div className="container-800">
+              <div className="texto">
+                <h1 className="title">SOLICITAR UM LEVANTAMENTO </h1>
+                <p className="description">
+                  O valor solicitado sera transferido para a plataforma
+                  principal, <span>EMAINVEST.AO</span>
+                  certifique-se CONSULTAR a transferencia.
+                </p>
               </div>
+
+              <div className="boxMoney">
+                <h3>Informe a quantidade por favor!</h3>
+                <form className="form">
+                  <div className="form-control">
+                    <input type="text" value={"Ao"} />
+                    <input
+                      id="getMoney"
+                      required
+                      type="number"
+                      name="getMoneyf"
+                      placeholder="2.000.000,00"
+                    />
+                  </div>
+                  <span id="la" className="">
+                    sssss
+                  </span>
+                  <button type="button" id="btn-getMoney" className="btn-busca">
+                    LEVANTAR
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
         </section>
 
         <section className="tabelaReferencia padding">
           <div className="container">
             <h2>Registro das transferencia</h2>
-           
+
             <div className="tabela__result">
               <table>
                 <thead>
@@ -56,7 +67,6 @@ export default function GetMoney(){
                 </thead>
                 <tbody>
                   <tr>
-              
                     <td>Nerd Photo</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -65,10 +75,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Nerd Photo</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -77,10 +85,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Banco bai</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -89,10 +95,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Emainvest</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -101,10 +105,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Coca cola</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -113,10 +115,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Coca cola</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -125,10 +125,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Coca cola</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -137,10 +135,8 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                   <tr>
-              
                     <td>Coca cola</td>
                     <td>Ao</td>
                     <td>124.000,00</td>
@@ -149,7 +145,6 @@ export default function GetMoney(){
                     <td>setembro</td>
                     <td>2022</td>
                     <td>7855sa4c4d</td>
-                 
                   </tr>
                 </tbody>
               </table>
@@ -162,29 +157,33 @@ export default function GetMoney(){
             <div className="paginas">
               <span>Prev</span>
               <span>1</span>
-              <span className="active" >2</span>
+              <span className="active">2</span>
               <span>3</span>
               <span>Next</span>
             </div>
 
             <div className="impressao">
               <form action="">
-                  <div className="form-control">
-                    <input type="number" placeholder="10" name="" id="" min={0} max={25} />
-                    <h4>Max<span>25</span></h4>
-                  </div>
-                  <button className="btn btn-primary" >imprimir</button>  
+                <div className="form-control">
+                  <input
+                    type="number"
+                    placeholder="10"
+                    name=""
+                    id=""
+                    min={0}
+                    max={25}
+                  />
+                  <h4>
+                    Max<span>25</span>
+                  </h4>
+                </div>
+                <button className="btn btn-primary">imprimir</button>
               </form>
+            </div>
           </div>
-
-          </div>
-
-          
         </section>
-
-
       </div>
-    {/* <Script src="scripts/main.js" />
+      {/* <Script src="scripts/main.js" />
     <Script src="scipts/java.min.js" /> */}
     </>
   );
